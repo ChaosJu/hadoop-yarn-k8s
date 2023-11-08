694712e79706338251d481899512cd414504988f
juchaochao <juchaochao@163.com>
2023-11-08 16:27:58 +0800

    add jmx exporter to nn and rm, proxy it

--- images/build.sh
+++ images/build.sh
@@ -7,5 +7,4 @@ docker build -t hadoop-namenode namenode
 docker build -t hadoop-datanode datanode
 docker build -t hadoop-nodemanager nodemanager
 docker build -t hadoop-resourcemanager resourcemanager
-
 docker build -t spark spark
